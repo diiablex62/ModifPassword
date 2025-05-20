758f5ab972ea16362b5ba600a4002bb3fe2b67af
Aleexj <jalias59@outlook.fr>
2025-05-20 11:48:09 +0200

    Modification de l'URL de base pour utiliser l'environnement de production ou de développement. Ajout d'un log pour afficher l'URL de base utilisée.

--- client_modifPassword/src/utils/url.js
+++ client_modifPassword/src/utils/url.js
@@ -1,1 +1,5 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = import.meta.env.PROD
+  ? "https://modifpassword-backend.onrender.com" // URL du backend
+  : "http://localhost:3000";
+
+console.log("URL de base utilisée:", BASE_URL);
